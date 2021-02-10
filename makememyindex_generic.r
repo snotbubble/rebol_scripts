00615b64e324734010ef43c27486b5cf3e7f0bc4
Craig Brown <36560021+snotbubble@users.noreply.github.com>
2021-02-10 08:51:01 +0000

    Add files via upload

---
+++ makememyindex_generic.r
@@ -0,0 +1,26 @@
+REBOL [
+	Title: makememyindex
+]
+
+site: "sitename.com"
+user: "username"
+newsite: "newsiteurl"
+
+h: {<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta http-equiv="refresh" content="0; URL='[newsite]'" />
+	</head>
+</html>}
+h: replace h "[newsite]" newsite
+
+i: %index.html
+write i h
+
+either exists? (to-file rejoin ["http://www." site "/index.php"]) [
+	pass: ask "password: "
+	either pass <> none and pass <> "" [
+		;write/binary join ["ftp://" user ":" pass "@ftp." site "/index.html"] read/binary i
+		;delete join ["ftp://" user ":" pass "@ftp." site "/index.php"]
+	] [ print "invalid password" ]
+] [ print "we're not fighting anybody today..." ]
